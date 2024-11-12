0641a984ad9f1c37b11f96111328013ff75c5d99
AmNat789 <amit.amanna@code.berlin>
2024-11-12 20:04:09 +0100

    add firebase admin auth

--- src/api/main.py
+++ src/api/main.py
@@ -1,44 +1,44 @@
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
-from functools import lru_cache
-
-from fastapi import FastAPI
+from typing import Annotated
+import firebase_admin
+from firebase_admin import credentials
+from fastapi import Depends, FastAPI
 from neo4j import AsyncGraphDatabase
-
-from src.config import settings
-from src.api.routers import query
-
-
-@lru_cache()
-def get_settings() -> settings.Settings:
-    # Use lru_cache to avoid loading .env file for every request
-    config = settings.Settings()
-    return config
-
+from config.settings import get_firebase_user_from_token, get_settings
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     """Async context manager for MongoDB connection."""
     settings = get_settings()
-    URI = f"bolt://{settings.neo4j_service}:7687"
-    AUTH = (settings.neo4j_user, settings.neo4j_password)
+    URI = f"bolt://{settings.NEO4J_SERVICE}:7687"
+    AUTH = (settings.NEO4J_USER, settings.NEO4J_PASSWORD)
     async with AsyncGraphDatabase.driver(URI, auth=AUTH) as driver:
         async with driver.session(database="neo4j") as session:
             app.session = session
-            print("Successfully connected to wine reviews Neo4j DB")
+            print("Successfully connected to Neo4j DB")
             yield
-            print("Successfully closed wine reviews Neo4j connection")
+            print("Successfully closed Neo4j connection")
 
 
 app = FastAPI(
-    title="REST API for wine reviews on Neo4j",
-    description=(
-        "Query from a Neo4j database of 130k wine reviews from the Wine Enthusiast magazine"
-    ),
-    version=get_settings().tag,
+    title="REST API for MTG Synchronizer",
+    version=get_settings().TAG,
     lifespan=lifespan,
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=[get_settings().FRONTEND_URL],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+cred = credentials.Certificate("service-account.json")
+firebase_admin.initialize_app(cred)
 
 @app.get("/", include_in_schema=False)
 async def root():
@@ -46,6 +46,7 @@ async def root():
         "message": "REST API for querying Neo4j database of 130k wine reviews from the Wine Enthusiast magazine"
     }
 
-
-# Attach routes
-app.include_router(query.router, prefix="/v1/query", tags=["query"])
+@app.get("/userid")
+async def get_userid(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
+    """gets the firebase connected user"""
+    return {"id": user["uid"]}
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,9 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from functools import lru_cache
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from firebase_admin.auth import verify_id_token
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -8,8 +12,47 @@ class Settings(BaseSettings):
         case_sensitive=False,
     )
 
-    neo4j_url: str
-    neo4j_service: str
-    neo4j_user: str
-    neo4j_password: str
-    tag: str
+    NEO4J_URL: str
+    NEO4J_SERVICE: str
+    NEO4J_USER: str
+    NEO4J_PASSWORD: str
+    TAG: str
+    FRONTEND_URL: str
+
+
+@lru_cache()
+def get_settings() -> Settings:
+    # Use lru_cache to avoid loading .env file for every request
+    config = Settings()
+    return config
+
+
+bearer_scheme = HTTPBearer(auto_error=False)
+def get_firebase_user_from_token(
+    token: Annotated[HTTPAuthorizationCredentials | None, Depends(bearer_scheme)]
+) -> dict | None:
+    """Uses bearer token to identify firebase user id
+    Args:
+        token : the bearer token. Can be None as we set auto_error to False
+    Returns:
+        dict: the firebase user on success
+    Raises:
+        HTTPException 401 if user does not exist or token is invalid
+    """
+    try:
+        if not token:
+            # raise and catch to return 401, only needed because fastapi returns 403
+            # by default instead of 401 so we set auto_error to False
+            raise ValueError("No token")
+        user = verify_id_token(token.credentials)
+        return user
+    # lots of possible exceptions, see firebase_admin.auth,
+    # but most of the time it is a credentials issue
+    except Exception:
+        # we also set the header
+        # see https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not logged in or Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
